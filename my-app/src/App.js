5a4ab27e0d4afaa1659a2145b5716aed80525732
Rubi Renteria <rubirenteria43@gmail.com>
2019-11-11 10:35:28 -0700

    added react router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,15 +2,30 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Jumbotron from "./components/Jumbotron";
 import Card from "./components/Card";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useHistory,
+  useLocation
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="container">
-      <Navbar />
-      
-      <Jumbotron />
-     
-    </div>
+    <Router>
+      <div className="container">
+        
+
+        <Jumbotron />
+        <switch>
+          <Route path="/auth/google">
+            <google-btn/>
+          </Route>
+        </switch>
+      </div>
+    </Router>
   );
 }
 
