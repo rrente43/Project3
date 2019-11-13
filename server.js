5e909efdc1d5f5e17328e9e7f3e1794650f09347
Rubi Renteria <rubirenteria43@gmail.com>
2019-11-12 21:26:08 -0700

    saving work

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require("express");
-// const authRoutes =require('./routes/api/auth-routes');
-// const passportSetup = require('./config/passport-setup');
+const authRoutes =require('./routes/api/auth-routes');
+const passportSetup = require('./config/passport-setup');
 
 const mongoose = require("mongoose");
-// const routes = require("./routes");
+const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -21,7 +22,7 @@ if (process.env.NODE_ENV === "production") {
 // Connect to the Mongo DB
 // mongoose.connect(
 //   process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist"
-// );
+// ); 
 
 // Start the API server
 app.listen(PORT, function() {
