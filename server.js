2179ba50903be80544d6d170209014f8b7b5b1bc
Rubi Renteria <rubirenteria43@gmail.com>
2019-11-13 21:05:12 -0700

    WORKING GOOGLE OAUTH

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-app.use(routes , authRoutes);
-// app.use('/auth', authRoutes);
+app.use('/auth', authRoutes);
+app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/projectSchema");
