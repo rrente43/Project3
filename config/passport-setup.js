683143b92bed1615766c8f729870375069b2942f
Rubi Renteria <rubirenteria43@gmail.com>
2019-11-14 09:54:22 -0700

    mongodb databse

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
+const User = require('../models/user-model');
 // var db = require("../models/")
 passport.use(
     new GoogleStrategy({
@@ -18,12 +19,12 @@ passport.use(
     //     if(currentUser){
     //         console.log('user is:', currentUser);
     //     }else{
-    //         new User({
-    //             username: profile.displayName,
-    //             googleId: profile.id
-    //         }).save().then((newUser)=>{
-    //         console.log('new user created'+ newUser);
-    //         });
+            new User({
+                username: profile.displayName,
+                googleId: profile.id
+            }).save().then((newUser)=>{
+            console.log('new user created'+ newUser);
+            });
     //     }
     // });
     
