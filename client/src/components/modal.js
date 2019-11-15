5528d32fa57d06773bea2faee23484ce869eba25
Christian Jimenez <cjjimenez0913@gmail.com>
2019-11-15 10:57:41 -0600

    added modal and form to modal

--- client/src/components/modal.js
+++ client/src/components/modal.js
@@ -1,5 +1,6 @@
 import React from "react";
-import {Modal, Button, ButtonToolbar} from 'react-bootstrap';
+import {Modal, Button, ButtonToolbar, Col, Row, Form, FormGroup, Label, Input} from 'react-bootstrap';
+
 
 
 function MyVerticallyCenteredModal(props) {
@@ -12,16 +13,60 @@ function MyVerticallyCenteredModal(props) {
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
+          Create Listing
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
+
+  <Form>
+    <Form.Row>
+      <Form.Group as={Col} controlId="formGridEmail">
+        <Form.Label>First Name</Form.Label>
+        <Form.Control type="email" placeholder="Enter First Name" />
+      </Form.Group>
+
+      <Form.Group as={Col} controlId="formGridPassword">
+        <Form.Label>Last Name</Form.Label>
+        <Form.Control type="password" placeholder="Enter Last Name" />
+      </Form.Group>
+    </Form.Row>
+
+    <Form.Group controlId="formGridAddress1">
+      <Form.Label>Address</Form.Label>
+      <Form.Control placeholder="1234 Main St" />
+    </Form.Group>
+
+    <Form.Group controlId="formGridAddress2">
+      <Form.Label>E-Mail</Form.Label>
+      <Form.Control placeholder="JohnDoe@roomr.com" />
+    </Form.Group>
+
+    <Form.Row>
+      <Form.Group as={Col} controlId="formGridCity">
+        <Form.Label>Occupation</Form.Label>
+        <Form.Control />
+      </Form.Group>
+
+      <Form.Group as={Col} controlId="formGridState">
+        <Form.Label>Age</Form.Label>
+        <Form.Control as="select">
+          <option>Choose...</option>
+          <option>...</option>
+        </Form.Control>
+      </Form.Group>
+
+      <Form.Group as={Col} controlId="formGridZip">
+        <Form.Label>Pets</Form.Label>
+        <Form.Control placeholder="#"/>
+      </Form.Group>
+    </Form.Row>
+
+    <Button variant="primary" type="submit">
+      Post
+    </Button>
+  </Form>
+
+
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
