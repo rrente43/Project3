47d96a6c28da92854f25dc4a8094114ad600c36a
Rubi Renteria <rubirenteria43@gmail.com>
2019-11-17 23:39:07 -0700

    working google api finally

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const passport = require('passport');
 const CLIENT_HOME_PAGE_URL = "http://localhost:3000/roommate";
 
+
 //auth login
 router.get('/login',(req,res)=>{
     res.render('login');
@@ -9,7 +10,9 @@ router.get('/login',(req,res)=>{
 
 router.get('/logout', (req,res)=>{
     //handle with passport
-    res.send("logging out");
+    // res.send("logging out");
+    req.logout();
+    res.redirect('/');
 });
 //auth google
 router.get('/google', passport.authenticate('google',{
@@ -17,11 +20,17 @@ router.get('/google', passport.authenticate('google',{
     
 }));
 
-//cb route for ggogle to redirect
-// router.get('/google/redirect', passport.authenticate('google'),(req,res)=>{
-//     // req.user
-//     // res.send(req.user );
-//     res.redirect('/profile/')
+
+router.get("/login/success", (req, res) => {
+    if (req.user) {
+      res.json({
+        success: true,
+        message: "user has successfully authenticated",
+        user: req.user,
+        cookies: req.cookies
+      });
+    }
+  });
 
 
 router.get(
@@ -29,9 +38,14 @@ router.get(
     passport.authenticate("google", {
       successRedirect: CLIENT_HOME_PAGE_URL,
       failureRedirect: "/auth/login/failed"
-    })
+    },
+    )
   );
 
+//   router.use(function(req, res) {
+//     res.sendFile(path.join(__dirname, "../client/public/index.html"));
+//   });
+  
 
 module.exports = router;
 
--- server.js
+++ server.js
@@ -36,6 +36,27 @@ app.use('/auth', authRoutes);
 // app.use('/profile', profileRoutes);
 // app.use(routes);
 
+const authCheck = (req, res, next) => {
+  if (!req.user) {
+    res.status(401).json({
+      authenticated: false,
+      message: "user has not been authenticated"
+    });
+  } else {
+    next();
+  }
+};
+
+app.get("/", authCheck, (req, res) => {
+  res.status(200).json({
+    authenticated: true,
+    message: "user successfully authenticated",
+    user: req.user,
+    cookies: req.cookies
+  });
+});
+
+
 // Connect to the Mongo DB
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/projectSchema");
 
